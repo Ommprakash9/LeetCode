e754f298b6628cf0067d5dfc9a3bcdf585a56f6e
Omm Prakash Sahoo <ommprakashsahoo1690@gmail.com>
2026-09-15 19:55:55 +0530

    Time: 0 ms (100%), Space: 9 MB (83.7%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -3,8 +3,8 @@ public:
     long long countCommas(long long n) {
         long long ans = 0;
 
-        for(long long x = 1000; x <= n; x *= 1000){
-            ans += n - x + 1;
+        for(long long a = 1000; a <= n; a *= 1000){
+            ans += n - a + 1;
         }
 
         return ans;
